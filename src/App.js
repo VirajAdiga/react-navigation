b4494f17e14543c3e7bf1dbf48dc36f1717d7033
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-05 16:03:45 +0530

    Create custom Button component to customize styling based on prop values

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <Button></Button>
+    <div>
+      <Button primary outline>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button success rounded>Success</Button>
+      <Button warning rounded>Warning</Button>
+      <Button danger outline>Danger</Button>
+      <Button outline rounded>Outline</Button>
     </div>
   );
 }
