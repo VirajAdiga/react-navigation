1ace372542e60cd962813c1035e49dc7ef70abe7
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-15 11:31:44 +0530

    Create page for button components reuse

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,40 @@
+import {GoBell, GoClockFill, GoHomeFill} from "react-icons/go";
+import { MdDangerous } from "react-icons/md";
+import Button from "../components/Button";
+
+function ButtonPage() {
+
+  const handleClick = () => {
+    console.log("Click!");
+  }
+
+  const handleMouseOver = () => {
+    console.log("Mouse Over!");
+  }
+
+  return (
+    <div>
+      <Button primary outline className="mb-5">
+        <GoBell />
+        Primary
+      </Button>
+      <Button secondary onClick={handleClick}>
+        <GoClockFill />
+        Secondary
+      </Button>
+      <Button success rounded>
+        <GoHomeFill />
+        Success
+      </Button>
+      <Button warning rounded onMouseOver={handleMouseOver}>Warning</Button>
+      <Button danger outline>
+        <MdDangerous />
+        Danger
+      </Button>
+      <Button outline rounded>Outline</Button>
+      <Button primary secondary>Error</Button>
+    </div>
+  );
+}
+
+export default ButtonPage;
