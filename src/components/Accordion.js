55d3a384e4b3a9c33e2f82960e2c0f16723f9872
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-22 12:05:59 +0530

    Add Accordion element

---
+++ src/components/Accordion.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
+
+function Accordion({ items }){
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const icon = <span>
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </span>
+
+        return (
+            <div key={item.id}>
+                <div onClick={() => handleClick(index)}>
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && <div>{item.content}</div>}
+            </div>
+        );
+    });
+
+    return (
+        <div>{renderedItems}</div>
+    )
+}
+
+export default Accordion;
