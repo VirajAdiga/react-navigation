03afeb54c967005eb394553eb64694fb2c9c095d
VirajAdiga <vadigathirthahall@deloitte.com>
2023-12-25 17:09:17 +0530

    Create Navigation context, links and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import Sidebar from "../src/components/Sidebar";
+import Route from "../src/components/Route";
+import AccordionPage from "../src/pages/AccordionPage";
+import ButtonPage from "../src/pages/ButtonPage";
+import DropdownPage from "../src/pages/DropdownPage";
+
 function App() {
   return (
-    <div>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import Link from "./Link";
+
+function Sidebar(){
+    const links = [
+        {label: "Dropdown", path: "/"},
+        {label: "Accordion", path: "/accordion"},
+        {label: "Buttons", path: "/buttons"}
+    ];
+
+    const renderedLinks = links.map((link) => {
+        return <Link className="mb-3 ml-3" activeClassName="font-bold border-l-4 border-blue-500 pl-2" key={link.label} to={link.path}>{link.label}</Link>
+    });
+
+    return (
+        <div className="sticky top-0 overflow-y-scroll flex flex-col items-start">{renderedLinks}</div>
+    )
+}
+
+export default Sidebar;
